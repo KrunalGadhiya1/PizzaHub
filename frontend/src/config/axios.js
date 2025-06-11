e868b9e44530bac64d2ceebe7f783a61b1dee4b6
KrunalGadhiya1 <139149145+KrunalGadhiya1@users.noreply.github.com>
2025-06-11 14:52:05 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with custom config
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://pizza-hub-chi.vercel.app/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
